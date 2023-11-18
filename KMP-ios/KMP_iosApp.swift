a6c2888dddfc515efdd4862329f8200dfedb75e2
VitaSokolova <vitasokolovavsu@gmail.com>
2023-11-18 12:33:28 +0100

    Initial Commit

---
+++ KMP-ios/KMP_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  KMP_iosApp.swift
+//  KMP-ios
+//
+//  Created by Sokolova Vita on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct KMP_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
