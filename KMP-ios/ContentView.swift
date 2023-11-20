f55f73b387097231a7f48455e1787bbc67240544
VitaSokolova <vitasokolovavsu@gmail.com>
2023-11-20 10:30:52 +0100

    integration with shared library

--- KMP-ios/ContentView.swift
+++ KMP-ios/ContentView.swift
@@ -6,19 +6,55 @@
 //
 
 import SwiftUI
+import shared
+
 
 struct ContentView: View {
+    @ObservedObject private(set) var viewModel: ViewModel
+    
+    @State private var userInput: String = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+             TextField("Enter Text", text: $userInput)
+                 .padding()
+                 .textFieldStyle(RoundedBorderTextFieldStyle())
+                 .onChange(of: userInput) { newValue in
+                     Task { await viewModel.fetchSearchResults(query: userInput)}
+                 }
+             
+            List(viewModel.phrases, id: \.self) { phrase in
+                Text(phrase)
+            }
+         }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(viewModel: ContentView.ViewModel())
+    }
+}
+
+extension ContentView {
+    class ViewModel: ObservableObject {
+        @Published var phrases: [String] = ["Loading..."]
+        
+        let repository = MoviesRepositoryImpl(httpClient:HttpClientHolder.init().client);
+        
+        init() {}
+        
+        func fetchSearchResults(query: String) async {
+            do {
+                let movies = try await repository.getSearchResults(query: query)
+                let titles = movies.map { $0.title }
+                
+                DispatchQueue.main.async {
+                    self.phrases = titles
+                }
+            } catch {
+                print("Error fetching search results: \(error.localizedDescription)")
+            }
+        }
+    }
 }
